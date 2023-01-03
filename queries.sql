260878c60b83c7a7e9765a746dce66552c17f9b3
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-03 20:26:11 +0100

    MAKE CHANGES RECOMMENDED BY THE REVIEWER

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '01/01/2016' AND '01/01/2019';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01/01/2016' AND '12/31/2019';
 SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
