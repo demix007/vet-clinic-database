435f0fd6d761ad9a6cb443cd7f6d253a558b2d26
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-07 02:35:07 +0100

    UPDATE ANIMALS TABLE

--- data.sql
+++ data.sql
@@ -31,4 +31,53 @@ VALUES
     ('Pokemon'),
     ('Digimon');
 
+/* Modify animals' species_id column */
+UPDATE animals 
+SET species_id = 2 
+WHERE name LIKE '%mon';
 
+UPDATE animals 
+SET species_id = 1  
+WHERE species_id IS NULL;
+
+/* Modify animals' owners_id column */
+
+UPDATE animals 
+SET owner_id = 1 
+WHERE name='Agumon';
+
+UPDATE animals 
+SET owner_id = 2 
+WHERE name='Gabumon';
+
+UPDATE animals 
+SET owner_id = 2 
+WHERE name='Pikachu';
+
+UPDATE animals 
+SET owner_id = 3 
+WHERE name='Devimon';
+
+UPDATE animals 
+SET owner_id = 3 
+WHERE name='Plantmon';
+
+UPDATE animals 
+SET owner_id = 4 
+WHERE name='Charmander';
+
+UPDATE animals 
+SET owner_id = 4 
+WHERE name='Squirtle';
+
+UPDATE animals 
+SET owner_id = 4 
+WHERE name='Blossom';
+
+UPDATE animals 
+SET owner_id = 5 
+WHERE name='Angemon';
+
+UPDATE animals 
+SET owner_id = 5 
+WHERE name='Boarmon';
