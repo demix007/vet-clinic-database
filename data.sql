37f483fe6b22a244e1ced10cf362796f8bb91552
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-07 04:54:33 +0100

    INSERT DATA INTO VETS TABLE

--- data.sql
+++ data.sql
@@ -86,3 +86,13 @@ UPDATE animals
 SET owner_id = (SELECT id from owners WHERE full_name = 'Dean Winchester')  
 WHERE name='Boarmon';
 
+/* Join Tables */
+
+/* Insert Data Into Vets Table */
+
+INSERT INTO vets (name, age, date_of_graduation) 
+VALUES 
+    ('William Tatcher', 45, '2000-04-23'),
+    ('Maisy Smith', 26, '2019-01-17'),
+    ('Stephanie Mendez', 64, '1981-05-04'),
+    ('Jack Harkness', 38, '2008-06-08');
