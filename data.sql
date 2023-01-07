226428d653459c6e2699d1fad07e42625e9ae02a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-07 02:30:19 +0100

    INSERT DATA INTO OWNERS TABLE

--- data.sql
+++ data.sql
@@ -14,3 +14,14 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Boarmon', date '2005-06-07', 7, true, 20.4);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Blossom', date '1998-10-13', 3, true, 17);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Ditto', date '2022-05-14', 4, true, 22);
+
+/* Insert data into owners table */
+INSERT INTO owners(full_name, age)
+VALUES 
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester',14 ),
+    ('Jodie Whittaker', 38);
+
