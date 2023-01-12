f91a74a34e2734b731b483331375d398d2a29907
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-01-12 13:46:41 +0100

    ADD QUERIES TO TEST DATABASE PERFORMANCE

--- queries.sql
+++ queries.sql
@@ -170,3 +170,9 @@ JOIN species ON animals.species_id = species.id
 WHERE vets.name='Maisy Smith' 
 GROUP BY species.name 
 ORDER BY count DESC LIMIT 1;
+
+/*  Database performance audit */
+
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
